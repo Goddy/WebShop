b0d526970779069d88c347f3ecd634e89c6a305b
Jan <jan.de.dobbeleer@gmail.com>
2015-01-10 21:28:22 +0100

    - Better text for order overview

--- WebShop/App_GlobalResources/Locale.Designer.cs
+++ WebShop/App_GlobalResources/Locale.Designer.cs
@@ -782,6 +782,15 @@ namespace WebShop.App_GlobalResources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Order overview.
+        /// </summary>
+        public static string OrderOverview {
+            get {
+                return ResourceManager.GetString("OrderOverview", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Orders.
         /// </summary>
         public static string Orders {
